5a2460539a6eba8a0d6490d60cd64cd69b732714
Daniel Cordero <github@0xdc.io>
2018-03-28 11:33:03 +0000

    Add test+dev default requirements

--- {{ cookiecutter.name }}/setup.py
+++ {{ cookiecutter.name }}/setup.py
@@ -9,10 +9,27 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+base_requirements = [
+    'django',
+]
+
+test_requirements = base_requirements + [
+    'coverage',
+]
+
+development_requirements = test_requirements + [
+    'pep8ify',
+]
+
 setup(
     name='{{ cookiecutter.name }}',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     long_description=README,
+    install_requires=base_requirements,
+    extras_require={
+        'test': test_requirements,
+        'dev': development_requirements,
+    },
 )
